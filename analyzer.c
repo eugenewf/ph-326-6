17c3012a2c7b962b225789e0d158b92aefa869cf
Eugene Foley <ewfoley@eden.rutgers.edu>
2012-11-12 18:06:36 -0500

    first round

--- analyzer.c
+++ analyzer.c
@@ -10,12 +10,12 @@ int main (int argc , char** argv){
 	double data[7][750];
 	int i  = 0;
 	char c;
-	double refpos;
+	double refpos,sdmrefpos,refang,sdmrefang;
 	double freq, sdmfreq,amp,sdmamp,vmax,sdmvmax, pshift,sdmpshift;
 
 	if(output==NULL){
 		output= fopen(argv[2], "w");
-		fprintf(output, "frequency\tsdm-freq\tAmplitude\tsdm-Amp\tvmax\tphase-shift\tsdm-shift\n");
+		fprintf(output, "filename\tangular-frequency\tsdm-freq\tAmplitude\tsdm-Amp\tvmax\tsdm-vmax\tphase-shift\tsdm-shift\tref position,\tsdm ref pos\n");
 	}else{
 		fclose( output);
 		fopen(argv[2], "a");
@@ -36,23 +36,61 @@ int main (int argc , char** argv){
 		}
 
 	}
-
-	refpos = averagepos(data);
+	averageang(data,&refang,&sdmrefang);
+	averagepos(data,&refpos,&sdmrefpos);
 	freqanal(data,&freq,&sdmfreq,refpos);
 	ampanal(data,&amp,&sdmamp,refpos);
+	sdmamp = sqrt(sdmamp*sdmamp+sdmrefpos*sdmrefpos);
 	vanal(&freq,&sdmfreq,&amp,&sdmamp,&vmax,&sdmvmax);
-	shiftanal(data,&pshift,&sdmpshift);
+	shiftanal(data,&pshift,&sdmpshift,refpos,refang);
+	pshift=((pshift*freq/(2*M_PI))+1)*(-360);
+	sdmpshift= sqrt(((sdmfreq)/(freq))*((sdmfreq)/(freq)) + ((sdmpshift)/(pshift))*((sdmpshift)/(pshift)));
 
-	fprintf(output, "%f\t%f\t%f\t%f\t%f\t%f\t%f\t%f\n", freq, sdmfreq, amp, sdmamp, vmax, sdmvmax, pshift, sdmpshift);
+	fprintf(output, "%s\t%f\t%f\t%f\t%f\t%f\t%f\t%f\t%f\t%f\t%f\n",argv[1], freq, sdmfreq, amp, sdmamp, vmax, sdmvmax, pshift, sdmpshift,refpos,sdmrefpos);
 }
-double averagepos(double data[7][750]){
+void averageang(double data[7][750],double*refpos,double*sdmrefpos){
 	int i =0;
+	double dat,ref;
+	double summation=0;
+	for (i=0;i<750;i++){
+		summation = summation + data[1][i];
+	}
+	printf("avpos = %g\n", summation / 750 );
+	*refpos= summation / 750;
+
+	summation=0;
+	ref= *refpos;
+	for(i=0;i<750;i++){
+		dat = data[1][i];
+		summation = summation + (dat-ref)*(dat-ref);
+	}
+	summation = summation / (750);
+	summation = summation / (749);
+
+	*sdmrefpos = sqrt(summation);
+
+}
+void averagepos(double data[7][750],double*refpos,double*sdmrefpos){
+	int i =0;
+	double dat,ref;
 	double summation=0;
 	for (i=0;i<750;i++){
 		summation = summation + data[4][i];
 	}
 	printf("avpos = %g\n", summation / 750 );
-	return summation / 750;
+	*refpos= summation / 750;
+
+	summation=0;
+	ref= *refpos;
+	for(i=0;i<750;i++){
+		dat = data[4][i];
+		summation = summation + (dat-ref)*(dat-ref);
+	}
+	summation = summation / (750);
+	summation = summation / (749);
+
+	*sdmrefpos = sqrt(summation);
+
 }
 void freqanal(double data[7][750],double*freq,double*sdmfreq,double refpos){
 	double sinks[100];
@@ -134,13 +172,70 @@ void ampanal (double data[7][750], double *amp , double *sdamp , double refpos){
 }
 
 
-void vanal(double *freq,double *sdmfreq,double *amp,double *sdmamp,double *vmax,double *sdmvma){
+void vanal(double *freq,double *sdmfreq,double *amp,double *sdmamp,double *vmax,double *sdmvmax){
+	*vmax=(*freq) * (*amp);
+	*sdmvmax = sqrt(((*sdmfreq)/(*freq))*((*sdmfreq)/(*freq))+((*sdmamp)/(*amp))*((*sdmamp)/(*amp)));
+
 
 }
-void shiftanal(double data[7][750],double* pshift,double* sdmpshift){
+void shiftanal(double data[7][750],double* pshift,double* sdmpshift,double refpos,double refang){
+	int i = 0;
+	int j =0;
+	int k =0;
+	double lastout,lastin;
+	double outsinks[100];
+	double insinks[100];
+	double shift[100];
+	double summation =0;
+	for (i=0;i<100;i++){
+		outsinks[i] = insinks[i] =0;
+		shift[i]=0;
+	}
 
+	lastout=-1;
+	lastin=-1;
+
+	for(i=0;i<750;i++){
+
+
+		if(lastout>=refpos && data[4][i]<refpos){
+			outsinks[j]=data[0][i-1];
+				j++;
+		}
+		if(lastin>=refang && data[1][i]<refang){
+			insinks[k]=data[0][i-1];
+			k++;
+		}
+		lastout=data[4][i];
+		lastin=data[1][i];
+	}
+	i=j=k=0;
+	if (insinks[0]<outsinks[0]){
+		j++;
+	}
+
+	while(insinks[j]!=0 && outsinks[i]!=0){
+		shift[k]=outsinks[i]-insinks[j];
+		i++;
+		j++;
+		k++;
+
+	}
+
+	for(i=0;i<k;i++){
+		summation = summation +shift[i];
+	}
+
+	*pshift= summation/k;
+	summation=0;
+	for(i=0;i<k;i++){
+		shift[i]=(shift[i]-*pshift)*(shift[i]-*pshift);
+		summation = summation + shift[i];
+	}
+	summation = summation / k;
+	summation = summation / (k-1);
+
+	*sdmpshift = sqrt(summation);
 }
-
-
 
 
--- analyzer.h
+++ analyzer.h
@@ -1,10 +1,12 @@
 #ifndef ANALYZER_H
 #define ANALYZER_H
 
-double averagepos(double data[7][750]);
+void averageang(double data[7][750],double*refpos,double*sdmrefpos);
+void averagepos(double data[7][750],double*refpos,double*sdmrefpos);
 void freqanal(double data[7][750],double*freq,double*sdmfreq,double refpos);
-void ampanal(double data[7][750],double*amp,double*sdamp,double*vmax,double refpos);
-void shiftanal(double data[7][750],double*pshift,double*sdmpshift);
+void ampanal (double data[7][750], double *amp , double *sdamp , double refpos);
+void vanal(double *freq,double *sdmfreq,double *amp,double *sdmamp,double *vmax,double *sdmvmax);
+void shiftanal(double data[7][750],double*pshift,double*sdmpshift,double refpos,double refang);
 
 
 #endif
